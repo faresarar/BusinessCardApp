df9caeda3f83108d619b43b3bb676e02b7c612b5
faresarar <faresarar455@gmail.com>
2024-07-15 19:37:09 +0100

    Container and Spacer widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class BusinessCardApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF2B475E),
+        backgroundColor: const Color(0xFF2B475E),
         body: Column(
           children: [
-            CircleAvatar(
+            const CircleAvatar(
               radius: 122,
               backgroundColor: Colors.white,
               child: CircleAvatar(
@@ -24,14 +24,70 @@ class BusinessCardApp extends StatelessWidget {
                 backgroundColor: Colors.black,
               ),
             ),
-            Text(
+            const Text(
               "Fares Arar",
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 32,
                 fontFamily: "Pacifico",
               ),
-            )
+            ),
+            const Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                  color: Color(0xFF6C8090),
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                  //color: Colors.white, you cant declare both of color and decoration
+                  height: 65,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 32,
+                        color: Color(0xFF2B475E),
+                      ),
+                      Text(
+                        "(+231) 0657248608",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      Spacer(flex: 1)
+                    ],
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                //color: Colors.white, you cant declare both of color and decoration
+                  height: 65,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.mail,
+                        size: 32,
+                        color: Color(0xFF2B475E),
+                      ),
+                      Text(
+                        "faresarar455@gmail.com",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      Spacer(flex: 1)
+                    ],
+                  )),
+            ),
+
           ],
         ),
       ),
