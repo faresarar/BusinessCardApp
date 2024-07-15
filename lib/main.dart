4448953b8537e6ebd386218b07c7c6407f463fb9
faresarar <faresarar455@gmail.com>
2024-07-15 16:23:10 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BusinessCardApp());
+}
+
+class BusinessCardApp extends StatelessWidget {
+  const BusinessCardApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
