0be7d8defff1c7c1982a0b9a80805baa55627117
faresarar <faresarar455@gmail.com>
2024-07-15 17:23:17 +0100

    Circle images and adding fonts

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,31 @@ class BusinessCardApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF2B475E),
+        body: Column(
+          children: [
+            CircleAvatar(
+              radius: 122,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/tharwat.png"),
+                radius: 120,
+                backgroundColor: Colors.black,
+              ),
+            ),
+            Text(
+              "Fares Arar",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontFamily: "Pacifico",
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
